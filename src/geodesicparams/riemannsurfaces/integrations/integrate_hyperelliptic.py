15a1aae86b0ec1a549f75c3c23b8b6aaef203aad
alexanderabesteh <alexander.abesteh@protonmail.com>
2024-10-06 20:29:51 -0400

    refactor(riemannsurfaces): move integration procedure
    Move the integration procedure found in periods_genus2_first to
    integrate_hyperelliptic.

--- src/geodesicparams/riemannsurfaces/integrations/integrate_hyperelliptic.py
+++ src/geodesicparams/riemannsurfaces/integrations/integrate_hyperelliptic.py
@@ -7,8 +7,8 @@ for periods of second kind will be changed soon (hopefully).
 
 """
 
-from mpmath import quad, matrix, exp, pi, atan2, cos, sin, mpc, mpf, binomial, mp
-from sympy import re, Symbol, collect, degree, lambdify, sqrt
+from mpmath import quad, matrix, exp, pi, atan2, cos, sin, mpc, mpf, binomial, mp, im, fabs
+from sympy import re, Symbol, collect, degree, lambdify, sqrt, oo
 
 from ..riemann_funcs.hyperelp_funcs import kleinian_zeta, kleinian_P
 from ...utilities import inlist, separate_zeros
@@ -414,146 +414,240 @@ def int_genus2_complex(zeros, realPart, imaPart, position, branch, digits):
     return matrix([a, realPart * a + 1j*b])
 
 def eval_period(m, n, realNS, zeros, omega, component = 0):
-    ﻿
-    lange = len(zeros)﻿
+    """
+    Computes the period from one real zero of the set of zeros defining a Riemann surface 
+    of genus 2 to another or to infinity.
 
-    if lange == 5:﻿
-        if len(realNS) == 3:﻿
-            if (im(zeros[1]) != 0 and im(zeros[2]) != 0): ﻿
-                e = [zeros[1], zeros[2], zeros[0], zeros[3], zeros[4]]﻿
-            elif (im(zeros[3]) != 0 and im(zeros[4]) != 0): ﻿
-                e = [zeros[0], zeros[1], zeros[3], zeros[4], zeros[2]]﻿
-            else:﻿
-                e = zeros﻿
-        elif len(realNS) == 1:﻿
-            if im(zeros[0]) == 0: ﻿
-                e = [zeros[1], zeros[2], zeros[3], zeros[4], zeros[0]]﻿
-            elif im(zeros[2]) == 0: ﻿
-                e  = [zeros[0], zeros[1], zeros[3], zeros[4], zeros[2]]﻿
-            else:﻿
-                e = zeros﻿
-        else:﻿
-            e = zeros﻿
+    Parameters
+    ----------
+    m : int
+        The index of the zero to compute the period from (realNS[<m>]).
+    n : int, oo
+        The index of the zero to compute the period to (realNS[<n>]). If <n> is oo, then
+        the period if computed from realNS[<m>] to oo.
+    realNS : list
+        The real zeros of the polynomial defining the genus 2 Riemann surface.
+    zeros : list
+        The zeros of the polynomial defining the genus 2 Riemann surface.
+    omega : matrix
+        A 2x4 mpmath matrix such that period_matrix = [<periods_first>, <periods_second>] (see
+        above).
+    component : int, optional
+        The row of the period matrix <omega> that should be used for the computation of the 
+        period (can either be 0 or 1, where the default value is 0). 
 
-    if m > n:﻿
-        k = n﻿
-        l = m﻿
-        sign = -1﻿
-    else:﻿
-        k = m﻿
-﻿        l = n﻿
-﻿        sign = 1﻿
-﻿﻿
-﻿    if k == -oo:﻿
-﻿        k = l﻿
-﻿        l = oo﻿
-﻿        sign = -sign﻿
-﻿﻿
-﻿    K = inlist(realNS[k], e)﻿
-﻿﻿
-﻿    if l < len(realNS):﻿
-﻿        L = inlist(realNS[l], e)﻿
-﻿    else:﻿
-﻿        L = lange﻿
-﻿﻿
-﻿    if component != 0 and component != 1:﻿
-﻿        print(f"WARNING in eval_period: illegal component {component} changed to 1")﻿
-﻿        component = 0﻿
-﻿﻿
-﻿    periodlist = [omega[component, 0], omega[component, 2] - omega[component, 3], omega[component, 1], omega[component, 3], -omega[component,0] - omega[component, 1]]﻿
-﻿    result = 0﻿
-﻿    for i in range(K, L):﻿
-﻿        result += periodlist[i]﻿
-﻿    result = sign * result﻿
-﻿﻿
-﻿    return result﻿
-﻿﻿
-﻿def branch_list_genus2(zeros, num_realNS):﻿
-﻿    if num_realNS == 5:﻿
-﻿        erg = [[1, zeros[0], 1], [1, zeros[1], 0], [0, zeros[2], 0], [0, zeros[3], 1], [1, zeros[4], 1]]﻿
-﻿    elif num_realNS == 3:﻿
-﻿        if im(zeros[0]) != 0:﻿
-﻿            erg = [[1, zeros[0], 1], [1, zeros[1], 0], [0, zeros[2], 0], [0, zeros[3], 1], [1, zeros[4], 1]]﻿
-﻿        elif im(zeros[1]) != 0:﻿
-﻿            erg = [["tbd", zeros[0], 0], [1, zeros[1], "tbd"], ["tbd", zeros[2], 0], [0, zeros[3], 1], [1, zeros[4], 1]]﻿
-﻿        elif im(zeros[2]) != 0:﻿
-﻿            rea1 = re(zeros[2])﻿
-﻿            if ((rea1-zeros[0]) * (rea1-zeros[1]) + (rea1-zeros[1]) * (rea1-zeros[4]) + (rea1-zeros[4]) * (rea1-zeros[0])).evalf() < 0:﻿
-﻿                erg = [[1, zeros[0], 1], [1, zeros[1], 0], [0, zeros[2], 1], [1, zeros[3], 1], [1, zeros[4], 1]]﻿
-﻿            else:﻿
-﻿                erg = [[1, zeros[0], 1], [1, zeros[1], 0], [0, zeros[2], 0], [0, zeros[3], 1], [1, zeros[4], 1]]﻿
-﻿        elif im(zeros[3]) != 0:﻿
-﻿            erg = [[1, zeros[0], 1], [1, zeros[1], 0], ["tbd", zeros[2], 1], [1, zeros[3], "tbd"], ["tbd", zeros[4], 1]]﻿
-﻿    elif num_realNS == 1:﻿
-﻿        if im(zeros[4]) == 0:﻿
-﻿            rea1 = re(zeros[0]); rea2 = re(zeros[2])﻿
-﻿            ima1 = fabs(im(zeros[0]))﻿
-﻿            if ((rea2-rea1)**2 + ima1**2 + 2 * (rea2-rea1) * (rea2-zeros[4])).evalf() < 0:﻿
-﻿                erg = [[1, zeros[0], 1], [1, zeros[1], 0], [0, zeros[2], 1], [1, zeros[3], 1], [1, zeros[4], 1]]﻿
-﻿            else:﻿
-﻿                erg = [[1, zeros[0], 1], [1, zeros[1], 0], [0, zeros[2], 0], [0, zeros[3], 1], [1, zeros[4], 1]]﻿
-﻿        elif im(zeros[0]) == 0:﻿
-﻿            erg = [["tbd", zeros[0], 1], [1, zeros[1], "tbd"], ["tbd", zeros[2], 1], [1, zeros[3], "tbd"], ["tbd", zeros[4], 1]]﻿
-﻿        elif im(zeros[2]) == 0:﻿
-﻿            erg = [[1, zeros[0], 1], [1, zeros[1], 0], ["tbd", zeros[2], 1], [1, zeros[3], "tbd"], ["tbd", zeros[4], 1]]﻿
-﻿    else:﻿
-﻿        raise ValueError("Wrong number of real zeros.")﻿
-﻿    return erg﻿
-﻿﻿
-﻿def int_genus2_first(zeros, lower, upper, digits, period_matrix):﻿
-﻿    if len(zeros) != 5:﻿
-﻿        raise Exception("Invalid use; number of zeros has to be 5.")﻿
-﻿﻿
-﻿    e = sorted(zeros, key = lambda x : re(x))﻿
-﻿    realNS, complexNS = separate_zeros(e)﻿
-﻿    ﻿
-﻿    if im(lower) != 0 and im(upper) != 0:﻿
-﻿        raise ValueError("Invalid use; only real integration bounds are feasible")﻿
-﻿﻿
-﻿    if lower == upper:﻿
-﻿        return 0﻿
-﻿    elif lower > upper:﻿
-﻿        sign = -1﻿
-﻿        lb = upper﻿
-﻿        ub = lower﻿
-﻿    else:﻿
-﻿        sign = 1﻿
-﻿        lb = lower﻿
-﻿        ub = upper﻿
-﻿﻿
-﻿    if inlist(lb, realNS) + 1 == inlist(ub, realNS):﻿
-﻿        if inlist(lb, e) + 1 == inlist(ub, e):﻿
-﻿            tags = (lb + ub) / 2﻿
-﻿        else:﻿
-﻿            tags = re(e[inlist(lb, e) + 1])﻿
-﻿        branch_list = branch_list_genus2(e, len(realNS))﻿
-﻿﻿
-﻿        return sign * (myint_genus2(e, lb, tags, branch_list[inlist(lb, e)][2], digits) + myint_genus2(e, tags, ub, branch_list[inlist(ub, e)][0], digits))﻿
-﻿﻿
-﻿    elif inlist(lb, realNS) >= 0 or inlist(ub, realNS) >= 0:﻿
-﻿        if inlist(lb, realNS) == -1:﻿
-﻿            if inlist(lb, sorted([lb] + realNS, key = lambda x : re(x))) == inlist(ub, realNS):﻿
-﻿                return sign * myint_genus2(e, lb, ub, branch_list_genus2(e, len(realNS))[inlist(ub, e)][0], digits)﻿
-﻿            else:﻿
-﻿                raise ValueError("Invalid bounds")﻿
-﻿        elif inlist(ub, realNS) == -1:﻿
-﻿            if inlist(ub, sorted([ub] + realNS, key = lambda x : re(x))) == inlist(lb, realNS) + 1:﻿
-﻿                return sign * myint_genus2(e, lb, ub, branch_list_genus2(e, len(realNS))[inlist(lb, e)][2], digits)﻿
-﻿            else:﻿
-﻿                raise ValueError("Invalid bounds")﻿
-﻿        else:﻿
-﻿            raise ValueError("Invalid bounds")﻿
-﻿    else:﻿
-﻿        if inlist(lb, sorted([lb] + realNS, key = lambda x : re(x))) == inlist(ub, sorted([ub] + realNS, key = lambda x : re(x))):﻿
-﻿            if ub == oo:﻿
-﻿                return sign * (myint_genus2(e, lb, realNS[-1], branch_list_genus2(e, len(realNS))[inlist(realNS[-1], e)][2], digits) + ﻿
-﻿                matrix([eval_period(len(realNS) - 1,oo,realNS,e,period_matrix,0), eval_period(len(realNS) - 1,oo,realNS,e,period_matrix,1)]))﻿
-﻿            else:﻿
-﻿                p = lambda x : (x - e[0]) * (x - e[1]) * (x - e[2]) * (x - e[3]) * (x - e[4])﻿
-﻿                sign = sign * exp(pi * 1j * branch_list_genus2(e, len(realNS))[inlist(lb, sorted(e + [lb], key = lambda x : re(x)))][2])﻿
-﻿                return matrix([sign * quad(lambda x : 1 / mpc(sqrt(p(x))), [lb, ub]), sign * quad(lambda x : x / mpc(sqrt(p(x))), [lb, ub])])﻿
-﻿        else:﻿
-﻿            raise ValueError("Invalid bounds")﻿
+    Returns
+    -------
+    result : complex
+        The period from realNS[<m>] to realNS[<n>]. 
+
+    """
+  
+    # Sort <zeros> by periodloops
+    lange = len(zeros)
+    if lange == 5:
+        if len(realNS) == 3:
+            if (im(zeros[1]) != 0 and im(zeros[2]) != 0): 
+                e = [zeros[1], zeros[2], zeros[0], zeros[3], zeros[4]]
+            elif (im(zeros[3]) != 0 and im(zeros[4]) != 0): 
+                e = [zeros[0], zeros[1], zeros[3], zeros[4], zeros[2]]
+            else:
+                e = zeros
+        elif len(realNS) == 1:
+            if im(zeros[0]) == 0: 
+                e = [zeros[1], zeros[2], zeros[3], zeros[4], zeros[0]]
+            elif im(zeros[2]) == 0: 
+                e  = [zeros[0], zeros[1], zeros[3], zeros[4], zeros[2]]
+            else:
+                e = zeros
+        else:
+            e = zeros
+
+    # Sort by m < n
+    if m > n:
+        k = n
+        l = m
+        sign = -1
+    else:
+        k = m
+        l = n
+        sign = 1
+
+    if k == -oo:
+        k = l
+        l = oo
+        sign = -sign
+
+    # Location of m, n in e
+    K = inlist(realNS[k], e)
+
+    if l < len(realNS):
+        L = inlist(realNS[l], e)
+    else:
+        L = lange
+
+    if component != 0 and component != 1:
+        print(f"WARNING in eval_period: illegal component {component} changed to 1")
+        component = 0
+
+    periodlist = [omega[component, 0], omega[component, 2] - omega[component, 3], omega[component, 1], omega[component, 3], -omega[component,0] - omega[component, 1]]
+    result = 0
+
+    # Compute period
+    for i in range(K, L):
+        result += periodlist[i]
+    result = sign * result
+
+    return result
+
+def branch_list_genus2(zeros, num_realNS):
+    """
+    Determines the branch adjacent to any zero given a list of zeros. 
+
+    Parameters
+    ----------
+    zeros : list
+        The 5 zeros of the polynomial defining the genus 2 Riemann surface.
+    num_realNS : list
+        The number of real zeros in <zeros>, which may be 1, 3, or 5.
+
+    Returns
+    -------
+    erg : list
+        A list containing 5 lists. Each element of the 5 lists consists of three parts: 
+        The first is the branch left of the zero, the second is the zero, and the third is 
+        the branch right of the zero. In the case that the second part is a complex zero, the 
+        entries depend on the sign of the imaginary part. If it is negative, the first entry 
+        is the branch left of the branch cut on the real axis and the third is the branch on 
+        the branch cut. If it's positive, the first entry is the branch on the branch cut and 
+        the third is the branch right of the branch cut on the real axis. 
+
+    """
+
+    if num_realNS == 5:
+        erg = [[1, zeros[0], 1], [1, zeros[1], 0], [0, zeros[2], 0], [0, zeros[3], 1], [1, zeros[4], 1]]
+    elif num_realNS == 3:
+        # ima2Per1
+        if im(zeros[0]) != 0:
+            erg = [[1, zeros[0], 1], [1, zeros[1], 0], [0, zeros[2], 0], [0, zeros[3], 1], [1, zeros[4], 1]]
+        # ima2Per2
+        elif im(zeros[1]) != 0:
+            erg = [["tbd", zeros[0], 0], [1, zeros[1], "tbd"], ["tbd", zeros[2], 0], [0, zeros[3], 1], [1, zeros[4], 1]]
+        # ima2Per3
+        elif im(zeros[2]) != 0:
+            rea1 = re(zeros[2])
+            if ((rea1-zeros[0]) * (rea1-zeros[1]) + (rea1-zeros[1]) * (rea1-zeros[4]) + (rea1-zeros[4]) * (rea1-zeros[0])).evalf() < 0:
+                erg = [[1, zeros[0], 1], [1, zeros[1], 0], [0, zeros[2], 1], [1, zeros[3], 1], [1, zeros[4], 1]]
+            else:
+                erg = [[1, zeros[0], 1], [1, zeros[1], 0], [0, zeros[2], 0], [0, zeros[3], 1], [1, zeros[4], 1]]
+        # ima2Per4
+        elif im(zeros[3]) != 0:
+            erg = [[1, zeros[0], 1], [1, zeros[1], 0], ["tbd", zeros[2], 1], [1, zeros[3], "tbd"], ["tbd", zeros[4], 1]]
+    elif num_realNS == 1:
+        # ima4Per1
+        if im(zeros[4]) == 0:
+            rea1 = re(zeros[0]); rea2 = re(zeros[2])
+            ima1 = fabs(im(zeros[0]))
+            if ((rea2-rea1)**2 + ima1**2 + 2 * (rea2-rea1) * (rea2-zeros[4])).evalf() < 0:
+                erg = [[1, zeros[0], 1], [1, zeros[1], 0], [0, zeros[2], 1], [1, zeros[3], 1], [1, zeros[4], 1]]
+            else:
+                erg = [[1, zeros[0], 1], [1, zeros[1], 0], [0, zeros[2], 0], [0, zeros[3], 1], [1, zeros[4], 1]]
+        # ima4Per2
+        elif im(zeros[0]) == 0:
+            erg = [["tbd", zeros[0], 1], [1, zeros[1], "tbd"], ["tbd", zeros[2], 1], [1, zeros[3], "tbd"], ["tbd", zeros[4], 1]]
+        # ima4Per3
+        elif im(zeros[2]) == 0:
+            erg = [[1, zeros[0], 1], [1, zeros[1], 0], ["tbd", zeros[2], 1], [1, zeros[3], "tbd"], ["tbd", zeros[4], 1]]
+    else:
+        raise ValueError("Wrong number of real zeros.")
+    return erg
+
+def int_genus2_first(zeros, lower, upper, digits, period_matrix):
+    """
+    Integrates the vector of canonical holomorphic differentials dz = [1/sqrt(P(z)), 
+    z/sqrt(P(z))] from <lower> to <upper>.
+
+    Parameters
+    ----------
+    zeros : list
+        The 5 zeros of the polynomial defining the genus 2 Riemann surface.
+    lower : real
+        The lower integration bound.
+    upper : real
+        The upper integration bound.
+    digits : int
+        The number of digits to use in the computation.
+    periodMatrix : matrix, optional
+        A 2x4 mpmath matrix such that periodMatrix = [<periods_first>, <periods_second>],
+        where <periods_first> is the period matrix of the integral of holomorphic differentals
+        taken along the contours around the branch cuts, while <periods_first> is the period
+        matrix integrated along the contours connecting branch cuts.
+
+    Returns
+    -------
+    result : list
+        The value of the two integrals as a list.
+
+    """
+
+    if len(zeros) != 5:
+        raise Exception("Invalid use; number of zeros has to be 5.")
+
+    e = sorted(zeros, key = lambda x : re(x))
+    realNS, complexNS = separate_zeros(e)
+    
+    if im(lower) != 0 and im(upper) != 0:
+        raise ValueError("Invalid use; only real integration bounds are feasible")
+
+    if lower == upper:
+        return 0
+    elif lower > upper:
+        sign = -1
+        lb = upper
+        ub = lower
+    else:
+        sign = 1
+        lb = lower
+        ub = upper
+
+    # ------------------ Case 1: lb and ub are adjacent real zeros
+    if inlist(lb, realNS) + 1 == inlist(ub, realNS):
+        if inlist(lb, e) + 1 == inlist(ub, e):
+            tags = (lb + ub) / 2
+        else:
+            tags = re(e[inlist(lb, e) + 1])
+        branch_list = branch_list_genus2(e, len(realNS))
+
+        return sign * (myint_genus2(e, lb, tags, branch_list[inlist(lb, e)][2], digits) + myint_genus2(e, tags, ub, branch_list[inlist(ub, e)][0], digits))
+    # ------------------ Case 2: only one of lb and ub is a real zero
+    elif inlist(lb, realNS) >= 0 or inlist(ub, realNS) >= 0:
+        if inlist(lb, realNS) == -1:
+            # No real zeros between lb and ub
+            if inlist(lb, sorted([lb] + realNS, key = lambda x : re(x))) == inlist(ub, realNS):
+                return sign * myint_genus2(e, lb, ub, branch_list_genus2(e, len(realNS))[inlist(ub, e)][0], digits)
+            else:
+                raise ValueError("Invalid bounds")
+        # Case 2b: lb is a real zero
+        elif inlist(ub, realNS) == -1:
+            # No real zeros between lb and ub
+            if inlist(ub, sorted([ub] + realNS, key = lambda x : re(x))) == inlist(lb, realNS) + 1:
+                return sign * myint_genus2(e, lb, ub, branch_list_genus2(e, len(realNS))[inlist(lb, e)][2], digits)
+            else:
+                raise ValueError("Invalid bounds")
+        else:
+            raise ValueError("Invalid bounds")
+    # ---------------- Case 3: none of lb or ub is a real zero
+    else:
+        if inlist(lb, sorted([lb] + realNS, key = lambda x : re(x))) == inlist(ub, sorted([ub] + realNS, key = lambda x : re(x))):
+            if ub == oo:
+                return sign * (myint_genus2(e, lb, realNS[-1], branch_list_genus2(e, len(realNS))[inlist(realNS[-1], e)][2], digits) + 
+                matrix([eval_period(len(realNS) - 1,oo,realNS,e,period_matrix,0), eval_period(len(realNS) - 1,oo,realNS,e,period_matrix,1)]))
+            else:
+                p = lambda x : (x - e[0]) * (x - e[1]) * (x - e[2]) * (x - e[3]) * (x - e[4])
+                sign = sign * exp(pi * 1j * branch_list_genus2(e, len(realNS))[inlist(lb, sorted(e + [lb], key = lambda x : re(x)))][2])
+                return matrix([sign * quad(lambda x : 1 / mpc(sqrt(p(x))), [lb, ub]), sign * quad(lambda x : x / mpc(sqrt(p(x))), [lb, ub])])
+        else:
+            raise ValueError("Invalid bounds")
 
 def int_second_genus2(zeros, pole, periods_first, periods_second, digits, minMax = 5):
     """
