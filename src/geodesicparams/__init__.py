3bc72524802bee3c560fbff37dd962255561c8b8
alexanderabesteh <alexander.abesteh@protonmail.com>
2024-10-27 00:10:40 -0400

    fix: fix import statement in __init__.py

--- src/geodesicparams/__init__.py
+++ src/geodesicparams/__init__.py
@@ -1,4 +1,4 @@
 from .ellipsefitting import *
 from .utilities import clear_directory
-from .fit_geodesic import fit_geodesic_orbit, compute_initials
+from .geodesicfitting import fit_geodesic_orbit
 from .solvegeodesics import *
