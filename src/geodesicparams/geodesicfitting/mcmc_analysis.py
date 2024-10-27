a1b6911be409cbfd8d74c473a9ce20f0de8eafd4
alexanderabesteh <alexander.abesteh@protonmail.com>
2024-10-26 23:46:15 -0400

    fix(geodesicfitting): add new parameters and gaussian priors
    Add new parameters to each function in order to account for changes in
    the methodology for computing the analytic solutions to the geodesics.
    Also, Gaussian priors were added for the MCMC analysis. Finally,
    docstrings and other documentation was also added.

--- src/geodesicparams/geodesicfitting/mcmc_analysis.py
+++ src/geodesicparams/geodesicfitting/mcmc_analysis.py
@@ -1,29 +1,41 @@
 #!/usr/bin/env python3
 """
+A collection of procedures for computing the MCMC log probability function.
 
+A SciPy log likelihood function is also used in order to compute the Max Likelihood 
+Estimation of the parameters. This MLE is used in the MCMC log probability function.
 
 """
 
 from mpmath import linspace, matrix, re
 from numpy import load, isfinite, inf
-from sympy import degree, Poly, re as spre, pprint
+#from sympy import degree, Poly, re as spre, pprint
 
-from ..utilities import clear_directory, separate_zeros, eval_roots, inlist
-from ..solvegeodesics import solve_geodesic_orbit, four_velocity, classify_spacetime, get_allowed_orbits 
+from ..utilities import clear_directory #,separate_zeros, eval_roots, inlist
+from ..solvegeodesics import solve_geodesic_orbit #,four_velocity, classify_spacetime, get_allowed_orbits 
+from .coordinates import conv_schwarzs_to_cart, conv_cartesian_to_schwarzs
+from .orbital_conversions import convert_newtonian
 
+# Constants
+# In units of solar masses, astronomical units, and years
+gLightSpeed = 6.3283e4
+gGravConst = 39.46
+gPerm = 7.54125e-6
+gNut = 0
+gMagnetic = 0
+
+"""
 def check_r_theta(theta):
-    """
 
 
     Parameters
     ----------
-
+    theta : 
 
     Returns
     -------
 
 
-    """
 
     M, J, q_e, m, K, E, L = theta
     spacetime = classify_spacetime(q_e, J, cosmo, NUT)
@@ -55,58 +67,101 @@ def check_r_theta(theta):
     else:
         return orbit
 
-def log_likelihood_scipy(theta, x_real, y_real, z_real, ecc, semi_maj, config):
-    """
 
+"""
+
+def log_likelihood_scipy(theta, x_real, y_real, z_real, ecc, semi_maj, orbit_inc, sign_ang, 
+                         init_dirs, config):
+    """
+    Computes the log likelihood of the parameters in <theta>.
+
+    This function is used to produced the Max Likelihood Estimation using SciPy minimize. The
+    MLE is then used for the MCMC analysis.
 
     Parameters
     ----------
-
+    theta : list
+        A list containing the specific energy squared E, specfic angular momentum
+        L, specific carter constant K, electric charge of the larger body e_charge, angular 
+        momentum of the larger body a, and cosmological constant cosmo. 
+    x_real : list
+        The real x data to be fitted with the geodesic model.
+    y_real : list
+        The real y data to be fitted with the geodesic model.
+    z_real : list
+        The real z data to be fitted with the geodesic model.
+    ecc : float
+        The eccentricity of the ellipse from 0 < ecc < 1.
+    semi_maj : float
+        The semi major axis of the ellipse, where semi_maj > 0.  
+    orbit_inc : float
+        The orbital inclination of the geodesic in radians.
+    sign_ang : int
+        An integer, either +1 if the orbit is prograde or -1 if the orbit is retrograde.       
+    init_dirs : list
+       A list containing two integers, either +1 or -1. The first element in the list
+       represents the initial direction of the geodesic (-1 for towards the larger body, +1
+       for away from the larger body) while the second element represents the initial
+       direction of the polar motion (-1 for towards the southern hemisphere, and +1 for
+       towards the northern hemisphere).
+    config : list
+        A list containing 3 elements: the first is a string containing the path to the 
+        working directory, the second is a string containing the date, and the third is
+        the number of digits to be used in the computation.
 
     Returns
     -------
-
+    sum : float
+        The log likelihood of the parameter space <theta>.
 
     """
 
     workdir, date, digits = config
+   
+    # Determine astrophysical parameters
+    if len(theta) == 2:
+        e_charge, b_rot = theta
+        cosmo = 0
 
-    M, J, q_e, m, K, E, L = theta
-
-    #mu = G * (M + m)
-  #  L = m * sqrt(mu * semi_maj * (1 - ecc**2))
-   # E = -G * M * m / (2 * semi_maj)
-
-    possible_orbit = check_r_theta(theta)
-    if type(possible_orbit) == float:
-        return 1e20
-
-    inits = compute_initials(x_real[0], y_real[0], z_real[0])
-
-    sol = solve_geodesic_orbit(M, J, q_e, cosmo, NUT, q_m, c, G, eps_0, 1, E, L, K, m, possible_orbit, config, [0] + inits, inits_dir)
-    sol_r, sol_theta, sol_phi = sol
-    
-    if cosmo != 0:
-        periodMatrix = load(workdir + "temp/rdata_" + date + ".npy", allow_pickle = True)[0]
-        period = matrix(periodMatrix)[1, 0] / 2
+        energy, ang_mom, carter = convert_newtonian(ecc, semi_maj, orbit_inc, sign_ang, 
+                        b_rot, e_charge, gGravConst, gLightSpeed, gPerm)
     else:
-        periodMatrix = load(workdir + "temp/rdata_" + date + ".npy", allow_pickle = True)[0]
-        period = periodMatrix[0]
+        energy, ang_mom, carter, e_charge, b_rot, cosmo = theta
+    
+    #possible_orbit = check_r_theta(theta)
+    #if type(possible_orbit) == float:
+    #    return 1e20
+
+    # Initial r, theta, and phi coordinates
+    inits = conv_cartesian_to_schwarzs(x_real[0], y_real[0], z_real[0])
+
+    # Compute solutions to the geodesic equations
+    sol = solve_geodesic_orbit(b_rot, e_charge, cosmo, gNut, gMagnetic, gLightSpeed, 
+            gGravConst, gPerm, 1, energy, ang_mom, carter, "bound", config, inits, init_dirs)
+    sol_r, sol_theta, sol_phi = sol
+
+    # Load period matrix to determine a full revolution of the orbiting body
+    if cosmo != 0:
+        period_matrix = load(workdir + "temp/rdata_" + date + ".npy", allow_pickle = True)[0]
+        period = matrix(period_matrix)[1, 0]
+    else:
+        period_matrix = load(workdir + "temp/rdata_" + date + ".npy", allow_pickle = True)[0]
+        period = period_matrix[0]
 
     mino = linspace(0, 2 * re(period), 500)
 
     r_list = []
     theta_list = []
 
+    # Obtain theoretical data from the geodesic model
     for i in mino:
         r_list.append(sol_r(i))
         theta_list.append(sol_theta(i))
     phi_list = sol_phi(mino)
-    
-    clear_directory(workdir + "temp/") 
+   
+    x_theo, y_theo, z_theo = conv_schwarzs_to_cart(r_list, theta_list, phi_list, digits)
 
-    x_theo, y_theo, z_theo = conv_schwarzs_cart(r_list, theta_list, phi_list, digits)
-
+    # Compute log likelihood
     sum = 0
     for i in range(len(mino)):
         x_sum = x_real[i] - x_theo[i]
@@ -115,52 +170,101 @@ def log_likelihood_scipy(theta, x_real, y_real, z_real, ecc, semi_maj, config):
 
         sum += x_sum**2 + y_sum**2 + z_sum**2
 
-    print("sum = ", -sum)
-    return - sum * 1e-7
+    sum = -sum
 
-def log_likelihood_emcee(theta, x_real, y_real, z_real, E, L, config):
+    # Clear temporary files
+    clear_directory(workdir + "temp/") 
+
+    return sum
+
+def log_likelihood_emcee(theta, x_real, y_real, z_real, ecc, semi_maj, orbit_inc, sign_ang, 
+                         init_dirs, config):
     """
+    Computes the log likelihood of the parameters in <theta>.
 
+    This function is called by log_probability_emcee in order to compute the log
+    probability in the MCMC analysis.
 
     Parameters
     ----------
-
+    theta : list
+        A list containing the specific energy squared E, specfic angular momentum
+        L, specific carter constant K, electric charge of the larger body e_charge, angular 
+        momentum of the larger body a, and cosmological constant cosmo. 
+    x_real : list
+        The real x data to be fitted with the geodesic model.
+    y_real : list
+        The real y data to be fitted with the geodesic model.
+    z_real : list
+        The real z data to be fitted with the geodesic model.
+    ecc : float
+        The eccentricity of the ellipse from 0 < ecc < 1.
+    semi_maj : float
+        The semi major axis of the ellipse, where semi_maj > 0.  
+    orbit_inc : float
+        The orbital inclination of the geodesic in radians.
+    sign_ang : int
+        An integer, either +1 if the orbit is prograde or -1 if the orbit is retrograde.       
+    init_dirs : list
+       A list containing two integers, either +1 or -1. The first element in the list
+       represents the initial direction of the geodesic (-1 for towards the larger body, +1
+       for away from the larger body) while the second element represents the initial
+       direction of the polar motion (-1 for towards the southern hemisphere, and +1 for
+       towards the northern hemisphere).
+    config : list
+        A list containing 3 elements: the first is a string containing the path to the 
+        working directory, the second is a string containing the date, and the third is
+        the number of digits to be used in the computation.
 
     Returns
     -------
-
-
+    sum : float
+        The log likelihood of the parameter space <theta>.
+        
     """
     
     workdir, date, digits = config
 
-    M, J, q_e, m, K, L, E = theta
- 
-    inits = compute_initials(x_real[0], y_real[0], z_real[0])
+    # Determine astrophysical parameters
+    if len(theta) == 2:
+        e_charge, b_rot = theta
+        cosmo = 0
 
-    sol = solve_geodesic_orbit(M, J, q_e, cosmo, NUT, q_m, c, G, eps_0, 1, E, L, K, m, "bound", config, inits, inits_dir)
-    sol_r, sol_theta, sol_phi = sol
-   
-    if cosmo != 0:
-        periodMatrix = load(workdir + "temp/rdata_" + date + ".npy", allow_pickle = True)[0]
-        period = matrix(periodMatrix)[1, 0] / 2
+        energy, ang_mom, carter = convert_newtonian(ecc, semi_maj, orbit_inc, sign_ang, 
+                        b_rot, e_charge, gGravConst, gLightSpeed, gPerm)
     else:
-        periodMatrix = load(workdir + "temp/rdata_" + date + ".npy", allow_pickle = True)[0]
-        period = periodMatrix[0]
+        energy, ang_mom, carter, e_charge, b_rot, cosmo = theta
+    
+    # Initial r, theta, and phi coordinates
+    inits = conv_cartesian_to_schwarzs(x_real[0], y_real[0], z_real[0])
 
-    mino = linspace(0, 2 * period, 1000)
-    clear_directory(workdir + "temp/") 
+    # Compute solutions to the geodesic equations
+    sol = solve_geodesic_orbit(b_rot, e_charge, cosmo, gNut, gMagnetic, gLightSpeed, 
+            gGravConst, gPerm, 1, energy, ang_mom, carter, "bound", config, inits, init_dirs)
+    sol_r, sol_theta, sol_phi = sol
+    
+    # Load period matrix to determine a full revolution of the orbiting body 
+    if cosmo != 0:
+        period_matrix = load(workdir + "temp/rdata_" + date + ".npy", allow_pickle = True)[0]
+        period = matrix(period_matrix)[1, 0]
+    else:
+        period_matrix = load(workdir + "temp/rdata_" + date + ".npy", allow_pickle = True)[0]
+        period = period_matrix[0]
 
+    mino = linspace(0, 2 * period, 500)
+   
     r_list = []
     theta_list = []
-
+  
+    # Obtain theoretical data from the geodesic model
     for i in mino:
         r_list.append(sol_r(i))
         theta_list.append(sol_theta(i))
     phi_list = sol_phi(mino)
+    
+    x_theo, y_theo, z_theo = conv_schwarzs_to_cart(r_list, theta_list, phi_list, digits)
 
-    x_theo, y_theo, z_theo = conv_schwarzs_cart(r_list, theta_list, phi_list)
-
+    # Compute log likelihood
     sum = 0
     for i in range(len(mino)):
         x_sum = x_real[i] - x_theo[i]
@@ -168,57 +272,152 @@ def log_likelihood_emcee(theta, x_real, y_real, z_real, E, L, config):
         z_sum = z_real[i] - z_theo[i]
 
         sum += x_sum**2 + y_sum**2 + z_sum**2
+   
+    sum = - sum
 
-    return - sum
+    # Clear temporary files
+    clear_directory(workdir + "temp/") 
 
-def log_priors(theta, E, L):
+    return sum
+
+def gaussian_prior(param, mu, sigma):
     """
-
+    Compute the log of a Gaussian prior of a parameter <param> with expected value <mu> 
+    and standard deviation <sigma>.
 
     Parameters
     ----------
-
+    param : float
+        A parameter for which the Gaussian prior is to be computed.
+    mu : float
+        The expected value of <param>. 
+    sigma : float
+        The standard deviation of <param>.
 
     Returns
     -------
-
+    float
+        The log of the Gaussian prior of the parameter.
 
     """
 
-    M, J, q_e, m, K = theta
-    
-    if M <= 0 or J < 0 or K < 0 or m <= 0:
-        return - inf
-    
-    possible = check_r_theta(theta, E)
+    return -0.5 * (param - mu)**2/sigma**2
 
-    if not isfinite(possible):
-        return - inf
+
+def log_priors(theta, means, stdevs, ecc, semi_maj, orbit_inc, sign_ang):
+    """
+    Compute the log of the priors function used in the MCMC analysis.
+
+    A combination of uniform and gaussian priors are used when computing
+    the priors function. 
+
+    Parameters
+    ----------
+    theta : list
+        A list containing the specific energy squared E, specfic angular momentum
+        L, specific carter constant K, electric charge of the larger body e_charge, angular 
+        momentum of the larger body a, and cosmological constant cosmo.
+    means : list
+        The expected values of the parameters in the parameter space <theta>.
+    stdevs : list
+        The standard deviations of the parameters in the parameter space <theta>. 
+    ecc : float
+        The eccentricity of the ellipse from 0 < ecc < 1.
+    semi_maj : float
+        The semi major axis of the ellipse, where semi_maj > 0.  
+    orbit_inc : float
+        The orbital inclination of the geodesic in radians.
+    sign_ang : int
+        An integer, either +1 if the orbit is prograde or -1 if the orbit is retrograde.       
+    
+    Returns
+    -------
+    ln_priors : float
+        The log of the priors function.
+
+    """
+
+    # Determine astrophysical parameters
+    if len(theta) == 2:
+        e_charge, b_rot = theta
+        cosmo = 0
+
+        energy, ang_mom, carter = convert_newtonian(ecc, semi_maj, orbit_inc, sign_ang, 
+                        b_rot, e_charge, gGravConst, gLightSpeed, gPerm)
+        mod_theta = [energy, ang_mom, carter, e_charge, b_rot, cosmo]
     else:
-        return 0.0 
+        b_rot = theta[4]; carter = theta[2]
+        mod_theta = theta
+    
+    if b_rot < 0 or carter < 0:
+        return - inf
 
-def log_probability(theta, x, y, z, ecc, semi_maj, config):
+    #possible = check_r_theta(mod_theta)
+    ln_priors = 0
+
+    #if not isfinite(possible):
+    #    return - inf
+
+    for i in range(6):
+        if i != 0:
+            ln_priors += gaussian_prior(mod_theta[i], means[i], stdevs[i])
+
+    return ln_priors
+
+def log_probability_emcee(theta, means, stdevs, x_real, y_real, z_real, ecc, semi_maj, 
+                          orbit_inc, sign_ang, init_dirs, config):
     """
+    Compute the log probability function used in the MCMC analysis.
 
+    The log of the posterior probability distribution is the sum of the log of the priors 
+    function and log likelihood function.
 
     Parameters
     ----------
-
+    theta : list
+        A list containing the specific energy squared E, specfic angular momentum
+        L, specific carter constant K, electric charge of the larger body e_charge, angular 
+        momentum of the larger body a, and cosmological constant cosmo.
+    means : list
+        The expected values of the parameters in the parameter space <theta>.
+    stdevs : list
+        The standard deviations of the parameters in the parameter space <theta>.  
+    x_real : list
+        The real x data to be fitted with the geodesic model.
+    y_real : list
+        The real y data to be fitted with the geodesic model.
+    z_real : list
+        The real z data to be fitted with the geodesic model.
+    ecc : float
+        The eccentricity of the ellipse from 0 < ecc < 1.
+    semi_maj : float
+        The semi major axis of the ellipse, where semi_maj > 0.  
+    orbit_inc : float
+        The orbital inclination of the geodesic in radians.
+    sign_ang : int
+        An integer, either +1 if the orbit is prograde or -1 if the orbit is retrograde.       
+    init_dirs : list
+       A list containing two integers, either +1 or -1. The first element in the list
+       represents the initial direction of the geodesic (-1 for towards the larger body, +1
+       for away from the larger body) while the second element represents the initial
+       direction of the polar motion (-1 for towards the southern hemisphere, and +1 for
+       towards the northern hemisphere).
+    config : list
+        A list containing 3 elements: the first is a string containing the path to the 
+        working directory, the second is a string containing the date, and the third is
+        the number of digits to be used in the computation.
 
     Returns
     -------
-
+    float
+        The log probability of the parameter space <theta>.
 
     """
-
-    M, J, q_e, m, K, E = theta
-
-    mu = G * (M + m)
-    #E = - m * mu / (2 * semi_maj)#m * c**2
-    #L = m * sqrt(mu * semi_maj * (1 - ecc**2))
-
-    lp = log_priors(theta, E, L)
     
+    lp = log_priors(theta, ecc, semi_maj, orbit_inc, sign_ang, means, stdevs)
+
     if not isfinite(lp):
         return - inf
-    return lp + log_likelihood_emcee(theta, x, y, z, L, config)
+
+    return lp + log_likelihood_emcee(theta, x_real, y_real, z_real, ecc, semi_maj, orbit_inc, 
+                                     sign_ang, init_dirs, config)
