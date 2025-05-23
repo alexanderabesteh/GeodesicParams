046ff1b537cdebec579f8e60c3e22f61dbf5cb5c
alexanderabesteh <alexander.abesteh@protonmail.com>
2025-05-23 11:48:29 -0400

    test: add test cases for orbit fitting and simulation
    NOTE: test cases are not yet complete.

--- tests/geodesic_fitting_demo/fitting.py
+++ tests/geodesic_fitting_demo/fitting.py
@@ -6,7 +6,7 @@ from astroquery.jplhorizons import Horizons
 from astropy.coordinates import SkyCoord
 from numpy import array
 from mpmath import sqrt
-
+import matplotlib.pyplot as plt
 obj = Horizons(id='199', location='500@10',
                epochs={'start':'1920-01-01', 'stop':'2019-12-31',
                        'step':'1y'})
@@ -20,6 +20,27 @@ y = vecs_au.y.value.tolist()
 z = vecs_au.z.value.tolist()
 
 data_points = array(list(zip(x, y, z)))
+res = fit_ellipse_3d(data_points, 500)[0]
+
+x1 = res[:, 0]
+y2 = res[:, 1]
+z3 = res[:, 2]
+figure, axe = plt.subplots(subplot_kw = dict(projection = "3d"))
+#axe.set_xlim([-20, 20])
+#axe.set_ylim([-20, 20])
+#axe.set_zlim([-20, 20])
+
+axe.plot(x1, y2, z3, color = "red")
+axe.scatter(x, y, z)
+axe.set_title('Neptune Orbit Fitting from 1920 - 2019 in Astronomical Units')
+#out_hor = plt.Circle((0, 0), out, color = "red", fill = False)
+#in_hor = plt.Circle((0, 0), inner, color = "yellow", fill = False)
+#axe.add_patch(out_hor)
+#axe.add_patch(in_hor)
+#axe.plot(th2, th1)
+#axe.plot(x1, y1)
+#axe.plot(mino, phiList)
+plt.show()
 
 pathway = str(dirname(dirname(abspath(__file__))))
 workdir = pathway + "/tests/"
@@ -30,4 +51,4 @@ config = [workdir, date, 15]
 init_theta = [1, 0.8, 0, 1, 12, sqrt(0.95), 3]
 #init_theta = [1, 1, 0, 1, 1, 1, 1]
 steps = 5000
-print(fit_geodesic_orbit(data_points, init_theta, steps, config))
+#print(fit_geodesic_orbit(data_points, init_theta, steps, config))
--- tests/geodesic_orbit_demo/demo.py
+++ tests/geodesic_orbit_demo/demo.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from os.path import dirname, abspath
 
 pathway = str(dirname(dirname(abspath(__file__))))
-workdir = pathway + "/tests/"
+workdir = pathway + "/geodesic_orbit_demo/"
 date = (datetime.now()).strftime("%Y-%m-%d_%H-%M-%S")
 config = [workdir, date]
 
@@ -17,28 +17,28 @@ cos = np.vectorize(cos, "D")
 sin = np.vectorize(sin, "D")
 
 bh_mass = 1
-energy = sqrt(0.95)  #1 #1.1 #1.2
-rot = 0#0.8
+energy = 0.9  #1 #1.1 #1.2
+rot = 0.53#0.8
 ang_mom = 3#5 #2
 light = 1
 particle_light = 1
-orbittype = "terminating"
-initials = [0, 20, 0.85, 0.33]
+orbittype = "bound"
+initials = [0, 35, 0.85, 0.33]
 dir_initials = [1, 1]
 mCharge = 0
 perm = 1
 p_mass = 1
 grav = 1
 carter = 12
-eCharge = 0 #2
+eCharge = 0.8 #2
 cosmo = 0
 nut = 0
 
 out = 1 + sqrt(1 - rot**2)
 inner = 1 - sqrt(1 - rot**2)
 
-sol = solve_geodesic_orbit(bh_mass, rot, eCharge, cosmo, nut, mCharge, light, grav, perm, particle_light, energy, ang_mom, carter, p_mass, orbittype, config, initials, dir_initials)
-mino = np.linspace(0, 2.7, 100)
+sol = solve_geodesic_orbit(bh_mass, rot, eCharge, cosmo, nut, mCharge, light, grav, perm, particle_light, energy, ang_mom, carter, orbittype, config, initials, dir_initials)
+mino = np.linspace(0, 5.34, 500)
 
 #periods, g2, g3, int_init, inits = np.load(workdir + "temp/rdata_" + date + ".npy", allow_pickle = True)
 #mino = np.linspace(0, 2 * float(periods[0]), 500)
@@ -47,11 +47,15 @@ mino = np.linspace(0, 2.7, 100)
 rList = []
 thetaList = []
 #thetaList = sol[1](mino)
-phiList = sol[2](mino)
+#phiList = sol[2](mino)
+#tList = sol[3](mino)
+
+#tauList = sol[4](mino)
 
 for i in mino:
     rList.append(sol[0](i))
     thetaList.append(sol[1](i))
+phiList = sol[2](mino)
 
 y1 = rList * sin(phiList)
 th1 = rList * cos(thetaList)
@@ -75,20 +79,20 @@ for i in range(len(rList)):
     phiList[i] = float(nstr(re(phiList[i])))
 #print(phiList)
 
-figure, axe = plt.subplots()
-#figure, axe = plt.subplots(subplot_kw = dict(projection = "3d"))
+#figure, axe = plt.subplots()
+figure, axe = plt.subplots(subplot_kw = dict(projection = "3d"))
 #axe.set_xlim([-20, 20])
 #axe.set_ylim([-20, 20])
 #axe.set_zlim([-20, 20])
 axe.axhline(y=0, color='black')
 axe.axvline(x=0, color='black')
 
-#axe.plot(x, y, z)
+axe.plot(x, y, z)
 #out_hor = plt.Circle((0, 0), out, color = "red", fill = False)
 #in_hor = plt.Circle((0, 0), inner, color = "yellow", fill = False)
 #axe.add_patch(out_hor)
 #axe.add_patch(in_hor)
 #axe.plot(th2, th1)
-axe.plot(x1, y1)
+#axe.plot(x1, y1)
 #axe.plot(mino, phiList)
 plt.show()
